576069fc92e4eb433cd5e6479aa8d704e7c0696a
Akshat Agarwal <32091195+ak-ag@users.noreply.github.com>
2021-06-02 18:12:19 +0530

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -1,13 +1,5 @@
 """
-Paper: "Fast and Accurate Image Super Resolution by Deep CNN with Skip Connection and Network in Network"
-Author: Jin Yamanaka
-Github: https://github.com/jiny2001/dcscn-image-super-resolution
 
-Create Augmented training images
-
-Put your images under data/[your dataset name]/ and specify [your dataset name] for --dataset.
-
---augment_level 2-8: will generate flipped / rotated images
 
 """
 import os
