5458adcb9e6ffa5430d2b23b0004278a6c57305e
Akshat Agarwal <32091195+ak-ag@users.noreply.github.com>
2021-06-02 18:11:56 +0530

    Update sr.py

--- sr.py
+++ sr.py
@@ -1,29 +1,4 @@
 """
-Paper: "Fast and Accurate Image Super Resolution by Deep CNN with Skip Connection and Network in Network"
-Author: Jin Yamanaka
-Github: https://github.com/jiny2001/dcscn-image-super-resolution
-Ver: 2.0
-
-Apply Super Resolution for image file.
-
---file=[your image filename]: will generate HR images.
-see output/[model_name]/ for checking result images.
-
-Also you must put same model args as you trained.
-
-For ex, if you trained like below,
-> python train.py --scale=3
-
-Then you must run sr.py like below.
-> python sr.py --scale=3 --file=your_image_file_path
-
-
-If you trained like below,
-> python train.py --dataset=bsd200 --layers=8 --filters=96 --training_images=30000
-
-Then you must run sr.py like below.
-> python sr.py --layers=8 --filters=96 --file=your_image_file_path
-
 """
 
 import tensorflow.compat.v1 as tf
